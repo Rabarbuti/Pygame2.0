384543e6e62b5fed3f4f74a412b94b7226ab8d5a
Rabarbuti <78366430+Rabarbuti@users.noreply.github.com>
2022-11-27 19:52:55 -0300

    jogo finalizado

--- jogo.py
+++ jogo.py
@@ -77,17 +77,17 @@ def main_menu():
         tela_fundo = pygame.transform.scale(tela_fundo,(WIDTH, HEIGHT))
         draw_text('main menu', fonte, (255, 255, 255), screen, 20, 20)
         screen.blit(tela_fundo,(0,0))
+
         if button_1.collidepoint((mx, my)):
-            if click:
+            if pygame.mouse.get_pressed()[0] == True:
                 setando_jogo()
                 game()
         
         if button_3.collidepoint((mx, my)):
-            if click:
+            if pygame.mouse.get_pressed()[0] == True:
                 options()
         pygame.draw.rect(screen, (255, 0, 0), button_1)
         pygame.draw.rect(screen, (255, 0, 0), button_3)
-
         click = False
         for event in pygame.event.get():
             if event.type == QUIT:
@@ -112,19 +112,30 @@ def main_menu():
 
 #tela inicial que funciona a base do clique em alguma tecla do jogador
 def telaFinal():
-    screen.blit(imagem_fundo_inicial, (0, 0))
-
-    texto(screen, "Infelizmente você perdeu", 30, WIDTH / 2, HEIGHT - 200)
-
-    pygame.display.flip()
+    
     bandeira = True
     while bandeira:
+        screen.fill((0,0,0))
+        screen.blit(imagem_fundo_inicial, (0, 0))
+        texto(screen, "Infelizmente você perdeu", 30, WIDTH / 2, HEIGHT - 200)
+        fonte = pygame.font.SysFont(None, 40)
+        mx, my = pygame.mouse.get_pos()
+        button_4 = pygame.Rect(425, 600, 250, 45)
+        pygame.draw.rect(screen, (255, 0, 0), button_4)
+        draw_text('Jogar Novamente', fonte, (255, 255, 255), screen, 430, 610)
+        if button_4.collidepoint((mx, my)):
+            if pygame.mouse.get_pressed()[0] == True:
+                main_menu()
 
         for t in pygame.event.get():
             if t.type == pygame.QUIT:
                 pygame.quit()
             if t.type == pygame.KEYUP:
                 bandeira = False
+
+            pygame.display.update()
+        pygame.display.update()
+
 def options():
     running = True
     while running:
@@ -133,7 +144,7 @@ def options():
         font = pygame.font.SysFont(None, 40)
         mx, my = pygame.mouse.get_pos()
         button_3 = pygame.Rect(0,0, 100, 30)
-        tela_fundo = pygame.image.load('Regras.png')
+        tela_fundo = pygame.image.load('imagens/options.png')
         tela_fundo = pygame.transform.scale(tela_fundo,(WIDTH, HEIGHT))
         draw_text('main menu', font, (255, 255, 255), screen, 20, 20)
         draw_text('options', font, (255, 255, 255), screen, 20, 20)
@@ -162,6 +173,8 @@ def options():
 
 #loop principal do jogo
 def game():
+    pygame.display.update()
+    setando_jogo()
     pontuacao = 0
     global palavras_jogo, jogador_escrevendo, xp, yp, velocidade_da_palavra_medio
     xp = random.randint(100, WIDTH-100)
@@ -203,17 +216,18 @@ def game():
                         pontuacao +=len(palavras_jogo)
                         setando_jogo()
                         som_ponto.play()
+                        
                 else:
                     #jogador perdeu
                     som_perdeu.play()
                     telaFinal()
-                    time.sleep(3)
-                    pygame.quit()
+                    pygame.display.update()
         
         #colocando uma altura minima que a figura do personagem possa chegar, caso ultrapasse, ele ira perder
         if yp < HEIGHT - 80:
             pygame.display.update()
         else:
             telaFinal()
+            pygame.display.update()
 setando_jogo()
 main_menu()
